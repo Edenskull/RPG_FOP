00410a03f3b64d057b56befb86a1d37ef88324f1
Edenskull <maxchamp@live.fr>
2019-03-09 20:26:11 +0100

    Ennemies List

---
+++ plugins/GameSystem/ennemies.py
@@ -0,0 +1,16 @@
+ENNEMIES = [
+    {
+        "name": "Zombievore",
+        "pv": 10,
+        "atk": 10,
+        "money": 10,
+        "enrage": 5
+    },
+    {
+        "name": "DR. Froze",
+        "pv": 20,
+        "atk": 5,
+        "money": 50,
+        "enrage": 10
+    }
+]
