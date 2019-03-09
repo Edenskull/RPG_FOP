8fc302d7ad76a60a5fbc6f7185f940bf8afd52ab
Edenskull <maxchamp@live.fr>
2019-03-09 19:14:42 +0100

    Color System

---
+++ plugins/GameSystem/terminalcolor.py
@@ -0,0 +1,9 @@
+class TerminalColor:
+    HEADER = '\033[95m'  # Purple
+    OKBLUE = '\033[94m'  # Blue
+    OKGREEN = '\033[92m'  # Green
+    WARNING = '\033[93m'  # Yellow
+    FAIL = '\033[91m'  # Red
+    ENDC = '\033[0m'  # White
+    BOLD = '\033[1m'  # Bold
+    UNDERLINE = '\033[4m'  # Underline
