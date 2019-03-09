9ae372858ed26053618ce65cb4a4518efa8e65e6
Edenskull <maxchamp@live.fr>
2019-03-09 19:04:51 +0100

    Upgrade List

--- plugins/GameSystem/upgrade.py
+++ plugins/GameSystem/upgrade.py
@@ -1,44 +1,86 @@
 UPGRADES = [
     {
-        "name": "Point Attaque Up",
-        "mod": "pa",
-        "desc": "Point Attaque",
+        "name": "Life Point Up",
+        "mod": "pvmax",
+        "desc": "Life Point Max",
         "value": 2,
         "cost": 30
     },
     {
-        "name": "Point Attaque + Up",
-        "mod": "pa",
-        "desc": "Point Attaque +",
+        "name": "Life Point + Up",
+        "mod": "pvmax",
+        "desc": "Life Point Max",
         "value": 4,
-        "cost": 30
+        "cost": 50
     },
     {
-        "name": "Point Defense Up",
-        "mod": "pd",
-        "desc": "Point Defense",
+        "name": "Magic Point Up",
+        "mod": "mpmax",
+        "desc": "Magic Point Max",
         "value": 2,
         "cost": 30
     },
     {
-        "name": "Point Defense + Up",
-        "mod": "pd",
-        "desc": "Point Defense +",
+        "name": "Magic Point + Up",
+        "mod": "mpmax",
+        "desc": "Magic Point Max",
         "value": 4,
-        "cost": 30
+        "cost": 50
     },
     {
-        "name": "Point Concentration Up",
-        "mod": "pc",
-        "desc": "Point Concentration",
+        "name": "Attack Up",
+        "mod": "atk",
+        "desc": "Attack",
         "value": 2,
         "cost": 30
     },
     {
-        "name": "Point Concentration + Up",
-        "mod": "pc",
-        "desc": "Point Concentration +",
+        "name": "Attack + Up",
+        "mod": "atk",
+        "desc": "Attack",
         "value": 4,
+        "cost": 50
+    },
+    {
+        "name": "Magic Attack Up",
+        "mod": "matk",
+        "desc": "Magic Attack",
+        "value": 2,
+        "cost": 30
+    },
+    {
+        "name": "Magic Attack + Up",
+        "mod": "matk",
+        "desc": "Magic Attack",
+        "value": 4,
+        "cost": 50
+    },
+    {
+        "name": "Instant Heal",
+        "mod": "pv",
+        "desc": "Life Point",
+        "value": 10,
+        "cost": 20
+    },
+    {
+        "name": "Instant Heal +",
+        "mod": "pv",
+        "desc": "Life Point",
+        "value": 20,
+        "cost": 30
+    },
+    {
+        "name": "Instant Magic",
+        "mod": "mp",
+        "desc": "Magic Point",
+        "value": 10,
+        "cost": 20
+    },
+    {
+        "name": "Instant Magic +",
+        "mod": "mp",
+        "desc": "Magic Point",
+        "value": 20,
         "cost": 30
     }
 ]
